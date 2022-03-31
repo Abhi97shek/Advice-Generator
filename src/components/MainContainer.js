a2933d7a85ff919ab4b2108e7c7a8386cfbef150
Abhi97shek <abhi.kumar736@gmail.com>
2022-03-31 14:22:17 +0530

    Completed

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,35 +1,49 @@
-import React,{useEffect,useState,useCallback} from 'react'
+import React,{useEffect,useState} from 'react'
 import "../components/MainContainer.css";
 import patterDivider from "../assets/pd-desktop.svg";
 import dice from "../assets/icon-dice.svg";
 import axios from 'axios';
 const MainContainer = () => {
 
-    const [advice,setAdvice] = useState(null);
+    const [advice,setAdvice] = useState({});
+    const [isLoading,setIsLoading] = useState(false);
+
+    const fetchAPI = async ()=>{
+      const res = await axios.get("https://api.adviceslip.com/advice");
+      if(res.data)
+      {
+      setAdvice(res.data);
+      setIsLoading(true);
+      }
+     
+     
+      };
+
+      const adviceHandler =()=>{
+
+       fetchAPI();
+
+      };
 
     useEffect(()=>{
-
-        const fetchAPI = async ()=>{
-        const res = await axios.get("https://api.adviceslip.com/advice");
-        console.log(res.data);
-        setAdvice(res.data);
-        };
-
         fetchAPI();
-
-        // adviceHandler();
-
     },[]);
+
   return (
-    <div className='mainContainer'>
-        <div className='adviceContainer'>
-            <h1 className='adviceId'>ADVICE #117</h1>
-            <p className='advice'>"When you're at a concert or event, enjoy the moment, enjoy being there. Try leaving your camera in your pocket."</p>
-            <img className="divider" src={patterDivider} alt="patterDivider" />
-        </div>
-        <button className='btn'type='button' ><img src={dice} className="dice"></img></button>
-    </div>
+    <>
+   {isLoading && (
+     
+            <div className='mainContainer'>
+                <div className='adviceContainer'>
+                    <h1 className='adviceId'>ADVICE #{advice.slip.id || 117}</h1>
+                    <p className='advice'>"{advice.slip.advice || "Hello" }"</p>
+                    <img className="divider" src={patterDivider} alt="patterDivider"/>
+                </div>
+                <button className='btn'type='button' ><img src={dice} className="dice" onClick={adviceHandler}></img></button>
+            </div>
+   )}
+   </>
   )
 }
 
-export default MainContainer
+export default MainContainer;
